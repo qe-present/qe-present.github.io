c901a5aa603c4e969a588080ff05ad7d3c5bda9c
qe-present <2664481691@qq.com>
2024-07-04 23:44:16 +0800

    add

---
+++ assets/tool-D9ZRlb0_.js
@@ -0,0 +1,1 @@
+const r=e=>{switch(e){case"米":return"METER";case"千米":return"KILOMETER";case"METER":return"米";case"KILOMETER":return"千米";case"area-meter":return"平方米";case"area-kilometer":return"平方千米"}};export{r as c};
---
+++ assets/use-quasar-BMTQU7zY.js
@@ -0,0 +1,1 @@
+import{i as a,bi as r}from"./index-DOXL9Kd_.js";function u(){return a(r)}export{u};
